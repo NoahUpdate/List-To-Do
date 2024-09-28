d2bb976e1292d6e55a10c9736c9e59d77c0f59cc
EdWard <educodepath@gmail.com>
2024-09-28 18:16:04 -0500

    Finalmente instalado Taillwind, eslint, prettier, atoms design y la config para pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,61 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+import * as path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@images',
+        replacement: path.resolve(__dirname, 'src/assets/images')
+      },
+      {
+        find: '@icons',
+        replacement: path.resolve(__dirname, 'src/assets/icons')
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/components/pages')
+      },
+      {
+        find: '@atoms',
+        replacement: path.resolve(__dirname, 'src/components/atoms')
+      },
+      {
+        find: '@molecules',
+        replacement: path.resolve(__dirname, 'src/components/molecules')
+      },
+      {
+        find: '@templates',
+        replacement: path.resolve(__dirname, 'src/components/templates')
+      },
+      {
+        find: '@organisms',
+        replacement: path.resolve(__dirname, 'src/components/organisms')
+      },
+      {
+        find: '@utils',
+        replacement: path.resolve(__dirname, 'src/utils')
+      },
+      {
+        find: '@types',
+        replacement: path.resolve(__dirname, 'src/types')
+      },
+      {
+        find: '@hooks',
+        replacement: path.resolve(__dirname, 'src/hooks')
+      },
+      {
+        find: '@services',
+        replacement: path.resolve(__dirname, 'src/services')
+      },
+      {
+        find: '@configs',
+        replacement: path.resolve(__dirname, 'src/configs')
+      }
+    ]
+  }
 });
