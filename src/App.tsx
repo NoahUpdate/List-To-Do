2ddba0cd6ac20431ce79426a1a2b4bb00183c27e
EdWard <educodepath@gmail.com>
2024-09-28 17:47:44 -0500

    Agregando las config de eslint y prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-
-import "./App.css";
+import './App.css';
 
 function App() {
-	return (
-		<div className="fondo">
-			<div className="bg-custom w-[414px] h-[472px] rounded-[32px]">
-				{/* <div className="">
-					<h1>List To Do</h1>
-				</div> */}
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-my-image bg-cover bg-top bg-no-repeat h-screen w-screen flex items-center justify-center">
+      <div
+        className="bg-custom w-[414px] h-[604px] rounded-[32px]
+			 flex flex-col items-center text-white text-[3.2em] leading-[1.1] font-bold"
+      >
+        <p>GG KUTAMI</p>
+
+        <div className="">
+          <p>List To Do</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			backgroundColor: {
-				custom: " hsla(143, 44%, 55%, 0.6)",
-				"custom-white": "rgba(255, 255, 255, 0.2)",
-			},
-		},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'my-image': "url('/src/assets/images/kinich_frame.jpg')"
+      },
+      backgroundColor: {
+        custom: ' hsla(143, 44%, 55%, 0.65)',
+        'custom-white': 'rgba(255, 255, 255, 0.5)'
+      }
+    }
+  },
+  plugins: []
 };
