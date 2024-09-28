796b28669bef1db9c9428f2048005168181939d7
EdWard <educodepath@gmail.com>
2024-09-28 15:03:51 -0500

    Empezando con tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-    <div className="fondo">
-      <div className="card_main">
-        <div className="card_main2">
-				<div>
-					<a
-						href="https://vitejs.dev"
-						target="_blank"
-            >
-						<img
-							src={viteLogo}
-							className="logo"
-							alt="Vite logo"
-              />
-					</a>
-					<a
-						href="https://react.dev"
-						target="_blank"
-            >
-						<img
-							src={reactLogo}
-							className="logo react"
-							alt="React logo"
-              />
-					</a>
-				</div>
-			<h1>List To Do</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					Hiciste {count} clicks
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+		<div className="fondo">
+			<div className="bg-custom w-[414px] h-[472px] rounded-[32px]">
+				{/* <div className="">
+					<h1>List To Do</h1>
+				</div> */}
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			</div>
-              </div>
-              </div>
-
+		</div>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      
-    },
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			backgroundColor: {
+				custom: " hsla(143, 44%, 55%, 0.6)",
+				"custom-white": "rgba(255, 255, 255, 0.2)",
+			},
+		},
+	},
+	plugins: [],
+};
